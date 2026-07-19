fe3e96352594672e02af93ed04d7c1bb814da6ca
Romsi18 <mufaridan.rasyad17@gmail.com>
2026-07-19 22:20:29 +0700

    Menambah Jamur

--- src/components/FormKonsultasi.jsx
+++ src/components/FormKonsultasi.jsx
@@ -11,7 +11,7 @@ function FormKonsultasi({
   const opsiMainRasa = ["manis", "asin", "gurih", "pedas", "asam"];
   const opsiMainKarbo = ["nasi", "mie", "bihun", "kwetiau","bubur", "roti"];
   const opsiMainProtein = ["ayam", "sapi", "udang", "cumi", "ikan", "kerang", "kepiting", "bebek", "kambing", "telur", "tahu", "tempe", "bakso", "sosis", 
-    "pangsit", "sayur"];
+    "pangsit", "sayur","jamur"];
   const opsiBevRasa = ["manis", "asam", "pahit", "gurih", "segar"];
   const opsiSideRasa = ["manis", "asin", "gurih", "pedas", "pahit"];
 
